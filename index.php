8ccd653ec69d8a15617f6582d9d349e9f56afb8a
dagdelen <omerdagdelen@gmail.com>
2026-03-30 23:11:16 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
   <link href="<?=$favicon?>" rel="icon">
   <link href="<?=$apple_touch_icon?>" rel="apple-touch-icon">
 
- <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-  <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
-  <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
- 
+  <script src="assets/js/jquery-3.7.1.js"></script>
+  <script src="assets/js/1.14.1/jquery-ui.js"></script>
+  <link rel="stylesheet" href="assets/css/jquery-ui.css">
+
    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
@@ -56,26 +56,27 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
+  <script src="assets/js/moment.min.js"></script>
 
   <script src="assets/vendor/quill/quill.min.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
 
   <script src="assets/js/main.js"></script>
 
-<script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.2/js/dataTables.buttons.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.dataTables.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.print.min.js"></script>
+<script src="assets/cdn/dataTables.js"></script>
+<script src="assets/cdn/dataTables.buttons.js"></script>
+<script src="assets/cdn/buttons.dataTables.js"></script>
+<script src="assets/cdn/jszip.min.js"></script>
+<script src="assets/cdn/pdfmake.min.js"></script>
+<script src="assets/cdn/vfs_fonts.js"></script>
+<script src="assets/cdn/buttons.html5.min.js"></script>
+<script src="assets/cdn/buttons.print.min.js"></script>
 
-<link href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" rel="stylesheet">
-<link href="https://cdn.datatables.net/buttons/3.2.2/css/buttons.dataTables.css" rel="stylesheet">
+<link href="assets/cdn/dataTables.dataTables.css" rel="stylesheet">
+<link href="assets/cdn/buttons.dataTables.css" rel="stylesheet">
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="assets/cdn/bootstrap.min.js"></script> 
+
 
 <?php 
    include("mod/ana_modal.php"); 
